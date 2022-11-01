2b247b53afc748a4d2b4a8bc30f0801fb98322b9
gopikrishna456 <111789923+gopikrishna456@users.noreply.github.com>
2022-11-01 17:02:48 -0500

    completed

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var appRouter = require('./routes/onion');
+var gridbuildRouter = require('./routes/gridbuild');
+var selector=require('./routes/selector')
+
 
 var app = express();
 
@@ -21,6 +25,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/onion', appRouter);
+app.use('/gridbuild', gridbuildRouter);
+app.use('/selector', selector);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
